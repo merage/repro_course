6a90b4a516822a10d2b8782b735a57dbf6260d5c
Merage Ghane <mghane@vt.edu>
2017-03-15 12:04:33 -0400

    test whether data exists

--- metasearch_analysis.py
+++ metasearch_analysis.py
@@ -1,5 +1,13 @@
 # coding: utf-8
 # Download our data from openneu.ro
 # get_ipython().system('git clone https://github.com/OpenNeuroLab/metasearch.git')
-Path('data_not_in_repro').mkdir()
-mv metasearch/ data_not_in_repro/
+# Path('data_not_in_repro').mkdir()
+# mv metasearch/ data_not_in_repro/
+from pathlib import Path
+metasearch_dir = Path('data_not_in_repro/metasearch/')
+data_exists = metasearch_dir.exists()
+in_repro_course = Path.cwd().name == 'repro_course'
+if data_exists and in_repro_course:
+    print('Data directory has been downloaded')
+else:
+    print('Data has NOT been downloaded')
